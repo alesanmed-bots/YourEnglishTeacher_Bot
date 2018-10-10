8f6d5a698203c67835843909a44fd7e9082b33fe
Ale Sánchez <alesanchez@us.es>
2018-10-10 15:41:31 +0200

    Added oxford API
    Now the word data are retrieved from the Oxford API

--- bot/add_word.py
+++ bot/add_word.py
@@ -4,28 +4,37 @@ from os.path import dirname as dir
 
 path.append(dir(path[0]))
 
+import copy
 import connectors
+import telegram
 import telegram.ext as ext
+import requests
+import logging
+import os
+from connectors import oxford_api
 
-WORD, MEANING, MEANING_ERROR, LANGUAGE_ERROR = range(4)
+WORD, MEANING, MEANING_ERROR = range(3)
 _MEANING_ERROR_COUNTER_ = 0
 _NEW_WORD_ = None
 
 def main(dispatcher):
     add_word_handler = ext.ConversationHandler(
-        entry_points=[ext.CommandHandler('add_word', add_word)],
+        entry_points=[ext.CommandHandler('add_word', __add_word)],
 
         states={
-            WORD: [ext.MessageHandler(ext.Filters.text, set_word)],
-            MEANING: [ext.MessageHandler(ext.Filters.text, set_meaning)],
-            MEANING_ERROR: [ext.MessageHandler(ext.Filters.text, handle_meaning_error)]
+            WORD: [ext.MessageHandler(ext.Filters.text, __set_word)],
+            MEANING: [ext.MessageHandler(ext.Filters.text, __set_meaning),
+                      ext.CallbackQueryHandler(__word_meaning_verification, pattern=r'^word_meaning:\d$')
+                    ],
+            MEANING_ERROR: [ext.MessageHandler(ext.Filters.text, __handle_meaning_error)],
         },
 
-        fallbacks=[ext.CommandHandler('cancel', add_word_cancel)]
+        fallbacks=[ext.CommandHandler('cancel', __add_word_cancel)]
     )
+    
     dispatcher.add_handler(add_word_handler)
     
-def add_word(bot, update):
+def __add_word(bot, update):
     global _NEW_WORD_
     _NEW_WORD_ = {}
 
@@ -41,24 +50,84 @@ def add_word(bot, update):
 
     return next_state
 
-def set_word(bot, update):
+def __set_word(bot, update):
     global _NEW_WORD_
 
     _NEW_WORD_['word'] = update.message.text
 
-    update.message.reply_text("Nice. Now... tell me what that word means in spanish, please.")
+    next_state = MEANING
 
-    return MEANING
+    if connectors.words.check_duplicate_word(_NEW_WORD_['word']):
+        update.message.reply_text("Thank you for sending me a word, but I already have that word stored. Send me another one or /cancel, please")
 
-def set_meaning(bot, update):
+        next_state = WORD
+    else:
+        meaning_request = oxford_api.get_word_definition(_NEW_WORD_['word'])
+
+        if meaning_request.status_code == 200:
+            meaning_json = meaning_request.json()
+
+            __process_word(meaning_json)
+
+            message_text = 'Please, review the word meaning:\n'
+
+            for i, meaning in enumerate(_NEW_WORD_['meanings']):
+                line = '%s. (%s) %s\n\n' % (i+1, meaning['category'], meaning['definition'])
+
+                message_text += line
+            
+            message_text += '\nIs this correct?'
+
+            update.message.reply_text(message_text, reply_markup=__get_reply_keyboard())
+        else:
+            _NEW_WORD_['pronunciation'] = None
+            update.message.reply_text(
+                "Well, I'm having troubles retrieving the word meaning... " +
+                "please send me the word definition (in english, please)")
+
+    return next_state
+
+def __word_meaning_verification(bot, update):
+    global _NEW_WORD_
+
+    next_state = ext.ConversationHandler.END
+
+    callback = update.callback_query
+    callback_data = update.callback_query.data
+
+    definition_ok = int(callback_data.split(':')[-1])
+
+    callback.answer()
+    callback.edit_message_reply_markup()
+    
+    if definition_ok:
+        connectors.words.insert_word(_NEW_WORD_)
+
+        _NEW_WORD_ = None
+
+        callback.message.reply_text("Perfect, your word has been added!")
+        next_state = ext.ConversationHandler.END
+    else:
+        callback.message.reply_text("Well, in that case please send me the word meaning in english")
+        next_state = MEANING
+    
+    return next_state
+
+def __set_meaning(bot, update):
     global _NEW_WORD_
 
     next_state = ext.ConversationHandler.END
 
     word_meaning = update.message.text
 
+    _NEW_WORD_['from'] = 'manual_input'
+
     if word_meaning:
-        _NEW_WORD_['meaning'] = word_meaning
+        _NEW_WORD_['meanings'] = [{
+            'category': None,
+            'definition': word_meaning,
+            'example': None
+            }]
 
         connectors.words.insert_word(_NEW_WORD_)
 
@@ -70,7 +139,7 @@ def set_meaning(bot, update):
 
     return next_state
 
-def handle_meaning_error(bot, update):
+def __handle_meaning_error(bot, update):
     global _MEANING_ERROR_COUNTER_
     next_state = None
 
@@ -85,9 +154,82 @@ def handle_meaning_error(bot, update):
     return next_state
 
 
-def add_word_cancel(bot, update):
+def __add_word_cancel(bot, update):
     global _NEW_WORD_
 
     _NEW_WORD_ = None
 
-    update.message.reply_text("Ok, no word would be added")
+    update.message.reply_text("Ok, no word will be added")
+
+    return ext.ConversationHandler.END
+
+def __get_reply_keyboard():
+    keyboard = [
+        [telegram.InlineKeyboardButton('That\'s correct', callback_data='word_meaning:1')], 
+        [telegram.InlineKeyboardButton('No, it\'s not correct', callback_data='word_meaning:0')]
+    ]
+
+    return telegram.InlineKeyboardMarkup(keyboard)
+
+def __process_word(word_json):
+    lexical_entries = word_json['results'][0]['lexicalEntries']
+
+    meanings = []
+
+    for lexical_entry in lexical_entries:
+        meaning = {
+            'category': lexical_entry['lexicalCategory']
+        }
+
+        if 'pronunciation' not in _NEW_WORD_:
+            _NEW_WORD_['pronunciation'] = lexical_entry['pronunciations'][0]['phoneticSpelling']
+
+            __save_audio_file(lexical_entries)
+
+        for sense in lexical_entry['entries'][0]['senses']:
+            meaning['definition'] = sense['definitions'][0]
+
+            if 'examples' in sense:
+                meaning['example'] = sense['examples'][0]['text']
+            else:
+                meaning['example'] = None
+
+            meanings.append(copy.deepcopy(meaning))
+
+            meaning.pop('definition', None)
+            meaning.pop('example', None)
+
+    _NEW_WORD_['meanings'] = meanings
+
+    _NEW_WORD_['from'] = 'oxford'
+
+    return True
+            
+def __save_audio_file(lexical_entries):
+    global _NEW_WORD_
+
+    for lexical_entry in lexical_entries:
+        for pronunciation in lexical_entry['pronunciations']:
+            if 'audioFile' in pronunciation:
+                audio_file_raw = requests.get(pronunciation['audioFile'])
+
+                try:
+                    audio_file_raw.raise_for_status()
+                except:
+                    logging.getLogger().info(
+                        'Audio could not be downloaded. URL: %s, Status: %s' % 
+                        (pronunciation['audioFile'], audio_file_raw.status_code))
+                    return False
+                
+                audio_file = open(
+                    os.path.normpath(os.path.join(
+                        os.path.dirname(os.path.realpath(__file__)), 
+                        '../assets/audio_files/', 
+                        '%s.mp3' % (_NEW_WORD_['word'].lower(),)
+                    )), 'wb')
+
+                for chunk in audio_file_raw.iter_content(100000):
+                    audio_file.write(chunk)
+                audio_file.close()
+                
+            return True
